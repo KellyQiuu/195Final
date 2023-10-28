2892699835a4447a536d4f672b34f188ac4baaeb
Rogers <yanghaochong@gmail.com>
2023-10-28 10:38:05 -0400

    Added details to User and added UserInterface

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,4 +1,45 @@
 package entity;
 
-public class User {
+import java.util.List;
+
+public class User implements UserInterface{
+
+    private final String name;
+    private final String password;
+    private final String ID;
+    private final String Email;
+    private final List<String> Courses;
+
+    User(String name, String password, String ID, String Email){
+        this.name = name;
+        this.password = password;
+        this.ID = ID;
+        this.Email = Email;
+        this.Courses = null; // Set to null when register in default. Can modify later.
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getID() {
+        return ID;
+    }
+
+    @Override
+    public String getEmail() {
+        return Email;
+    }
+
+    @Override
+    public List<String> getCourses() {
+        return Courses;
+    }
 }
