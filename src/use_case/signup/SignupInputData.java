477e810f06257bd09b191ae150d2ecb1e9b3cb5c
Escalloniales <cyguchi@gmail.com>
2023-11-09 00:36:21 -0500

    Added interface adapter and use case for signup

---
+++ src/use_case/signup/SignupInputData.java
@@ -0,0 +1,51 @@
+package use_case.signup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Application Business Rules layer .
+ */
+public class SignupInputData {
+    final private String username;
+    final private String password;
+    final private String id;
+    final private String email;
+    final private List<String> courses;
+    /**
+     * @param username the username of the new user
+     * @param password the password of the new user
+     * @param id the id of the new user
+     * @param email the email of the new user
+     * @param courses the courses that the new user currently enrolled.
+     */
+
+    public SignupInputData(String username, String password, String id, String email,
+                           ArrayList<String> courses) {
+        this.username = username;
+        this.password = password;
+        this.id = id;
+        this.email = email;
+        this.courses = courses;
+    }
+
+    String getUsername() {
+        return username;
+    }
+
+    String getPassword() {
+        return password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<String> getCourses() {
+        return courses;
+    }
+}
--- src/use_case/signup/SignupUserAccessInterface.java
+++ src/use_case/signup/SignupUserAccessInterface.java
@@ -1,4 +1,4 @@
-package use_case;
+package use_case.signup;
 
 import entity.User;
 
@@ -11,6 +11,10 @@ public interface SignupUserAccessInterface {
      * @param username string username of the User to check if it has been taken.
      * @return true if and only if the username has not been taken and the user can register with this name.
      */
+
+    boolean checkValidEmail(String username);
+
+
     boolean checkValidUsername(String username);
 
     /**
@@ -18,7 +22,4 @@ public interface SignupUserAccessInterface {
      * @param user newly created User object
      */
     void save(User user);
-
-
-
 }
