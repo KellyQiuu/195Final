3bffb03715b542ad0992d232c8b9ed542020175d
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-11 16:31:20 -0500

    Link to EmailService and add try click button.

--- src/interface_adapter/profile/ProfileController.java
+++ src/interface_adapter/profile/ProfileController.java
@@ -1,13 +1,33 @@
 package interface_adapter.profile;
 
+import use_case.email_user.EmailService;
 import use_case.profile.ProfileInputBoundary;
-public class ProfileController {
-	final ProfileInputBoundary profileInteractor;
+import entity.User;
+import view.ProfileView;
 
-	public ProfileController(ProfileInputBoundary profileInteractor) {
-		this.profileInteractor=profileInteractor;
+public class ProfileController {
+	private final ProfileInputBoundary profileInteractor;
+	private final EmailService emailService;
+	private final ProfileView profileView;
+
+	public ProfileController(ProfileInputBoundary profileInteractor, EmailService emailService, ProfileView profileView) {
+		this.profileInteractor = profileInteractor;
+		this.emailService = emailService;
+		this.profileView = profileView;
 	}
 
+	public void onConnectButtonClick(User recipient, String freeTextMessage) {
+		try {
+			// Assume EmailService.sendEmail throws an exception if email sending fails
+			EmailService.sendEmail(recipient, freeTextMessage);
+			// If email was sent successfully, show a success message
+			profileView.showSuccessMessage();
+		} catch (Exception e) {
+			// Log the exception and show an error message
+			e.printStackTrace();
+			profileView.showErrorMessage("Failed to send email. Please try again later.");
+		}
+	}
 	public void execute(String username) {
 		profileInteractor.execute(username);
 	}
