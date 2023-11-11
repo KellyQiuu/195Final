7b6499a2a3e6ea8ad1dc00dc4abaa39dba4bc4a6
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-11 17:00:04 -0500

    Add a "Connect" button. A new functionality need to be realised: after clicking the button "Connect", there should be an area allow user to edit free text and send it to another user with his/her personal info simultaneously.

--- src/view/ProfileView.java
+++ src/view/ProfileView.java
@@ -1,8 +1,8 @@
 package view;
 
 import interface_adapter.profile.ProfileController;
-import interface_adapter.profile.ProfileState;
 import interface_adapter.profile.ProfileViewModel;
+import entity.User;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,6 +15,10 @@ public class ProfileView extends JPanel implements ActionListener, PropertyChang
     public final String viewName = "profile";
 
     private final ProfileViewModel profileViewModel;
+
+    final JButton connectButton;
+
+    private JTextArea freeTextMessageArea;
 
     final JButton update;
     private final ProfileController profileController;
@@ -32,6 +36,14 @@ public class ProfileView extends JPanel implements ActionListener, PropertyChang
         update = new JButton(ProfileViewModel.UPDATE_BUTTON_LABEL);
         buttons.add(update);
 
+        connectButton = new JButton(ProfileViewModel.CONNECT_BUTTON_LABEL);
+        connectButton.addActionListener(this);
+        this.add(connectButton);
+
+        freeTextMessageArea = new JTextArea(5,20);
+        JScrollPane scrollPane = new JScrollPane(freeTextMessageArea);
+        this.add(scrollPane);
+
         update.addActionListener(
                 new ActionListener() {
                     @Override
@@ -41,6 +53,28 @@ public class ProfileView extends JPanel implements ActionListener, PropertyChang
         );
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == connectButton) {
+            User recipient = getRecipientUser();
+            String freeTextMessage = freeTextMessageArea.getText();
+            profileController.onConnectButtonClick(recipient, freeTextMessage);
+        }
+    }
+
+    private User getRecipientUser() {
+        // TODO: Replace with actual logic to get the recipient User object
+        // This could be from the selection in a list or any other component in your UI
+        return null; // Placeholder
+    }
+
+    public void showSuccessMessage() {
+        JOptionPane.showMessageDialog(this, "Email sent successfully.");
+    }
+
+    public void showErrorMessage(String s) {
+        JOptionPane.showMessageDialog(this, "Email send failed.");
+    }
 
 
     @Override
