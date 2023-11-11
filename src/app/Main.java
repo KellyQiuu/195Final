4428b9d03f80c1971a416bfeface315c73c37dc9
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-11 16:28:23 -0500

    Some initialization.

--- src/app/Main.java
+++ src/app/Main.java
@@ -10,13 +10,26 @@ import use_case.email_user.EmailService;
 
 
 public class Main {
+    //Initialization
+    //// This setup code would typically go in your main method or application initialization logic
+    //
+    //// Create the dependencies of the ProfileController
+    //ProfileInputBoundary profileInteractor = new ProfileInteractor(/* dependencies */);
+    //EmailService emailService = new EmailService(/* your SendGrid API key */);
+    //ProfileView profileView = new ProfileView(/* ProfileViewModel */, /* ProfileController */);
+    //
+    //// Create the ProfileController
+    //ProfileController profileController = new ProfileController(profileInteractor, emailService, profileView);
+    //
+    //// Now that ProfileController is created, you must set it in the ProfileView
+    //profileView.setController(profileController);
     public static void main(String[] args) throws Exception {
         ArrayList<String> courses = new ArrayList<>();
 
         User u=UserFactory.creatUser("User1","123","1","qiuwenyu2021@outlook.com",courses);
 
 
-        EmailService.sendEmail(u);
+        EmailService.sendEmail(u, freeTextMessage);
         SessionManagerInteractor sessionManager = new SessionManagerInteractor();
         User user = UserFactory.creatUser("Lynnesy", "wenyu.qiu@mail.utoronto.ca","0",
                 "wenyu.qiu@mail.utoronto.ca", new ArrayList<String>());
