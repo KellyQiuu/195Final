e0bf18995270f296033b8b27faf0e4193098d217
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-12 18:22:05 -0500

    Need to add a new functionality to type free text while sending a email to another user.

--- src/use_case/email_user/EmailService1.java
+++ src/use_case/email_user/EmailService1.java
@@ -1,5 +1,6 @@
 package use_case.email_user;
 
+import entity.User;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -7,11 +8,10 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpRequest.BodyPublishers;
 import org.json.JSONObject;
 import org.json.JSONArray;
-import entity.User;
 
 public class EmailService {
     private static final String SENDGRID_API_URL = "https://api.sendgrid.com/v3/mail/send";
-    private final String sendGridApiKey;
+    private String sendGridApiKey; // The API Key for SendGrid
 
     public EmailService(String apiKey) {
         this.sendGridApiKey = apiKey;
@@ -19,36 +19,16 @@ public class EmailService {
 
     public void sendEmail(User recipient, String freeTextMessage) throws Exception {
         JSONObject emailJson = new JSONObject();
+        emailJson.put("from", new JSONObject().put("email", "from_email@example.com")); // Replace with your 'from' address
 
-        // Set the sender's email
-        JSONObject fromObject = new JSONObject();
-        fromObject.put("email", "your-email@example.com"); // Replace with your sending email address
-        emailJson.put("from", fromObject);
-
-        // Set the recipient
-        JSONObject toObject = new JSONObject();
-        toObject.put("email", recipient.getEmail());
-
-        JSONArray tosArray = new JSONArray();
-        tosArray.put(toObject);
-
-        // Set the personalization
+        JSONArray personalizationsArray = new JSONArray();
         JSONObject personalizationObject = new JSONObject();
-        personalizationObject.put("to", tosArray);
+        personalizationObject.put("to", new JSONArray().put(new JSONObject().put("email", recipient.getEmail())));
+        personalizationObject.put("dynamic_template_data", new JSONObject().put("free_text", freeTextMessage));
+        personalizationsArray.put(personalizationObject);
+        emailJson.put("personalizations", personalizationsArray);
 
-        // Add dynamic template data here
-        JSONObject dynamicData = new JSONObject();
-        dynamicData.put("name", recipient.getName());
-        dynamicData.put("course", String.join(", ", recipient.getCourses())); // Assuming courses is a list of strings
-        dynamicData.put("free_text", freeTextMessage);
-        personalizationObject.put("dynamic_template_data", dynamicData);
-
-        JSONArray personalizations = new JSONArray();
-        personalizations.put(personalizationObject);
-        emailJson.put("personalizations", personalizations);
-
-        // Set the template ID
-        emailJson.put("template_id", "d-1234567890abcdef1234567890abcdef"); // Replace with your actual template ID
+        emailJson.put("template_id", "your-template-id"); // Replace with your SendGrid template ID
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -61,11 +41,10 @@ public class EmailService {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         if (response.statusCode() == 202) {
-            System.out.println("Email sent successfully.");
+            System.out.println("Email sent successfully to " + recipient.getEmail());
         } else {
             System.out.println("Failed to send email with status code: " + response.statusCode());
-            System.out.println("Response: " + response.body());
-            // You might want to throw an exception or handle the error appropriately
+            throw new Exception("Email sending failed: " + response.body());
         }
     }
 }
