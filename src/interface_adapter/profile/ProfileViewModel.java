803ec6ad1f7dc78d2ec1bcc83aa8cc915f9215d7
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-13 17:45:40 -0500

    ...

--- src/interface_adapter/profile/ProfileViewModel.java
+++ src/interface_adapter/profile/ProfileViewModel.java
@@ -4,7 +4,6 @@ import interface_adapter.ViewModel;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 public class ProfileViewModel extends ViewModel {
-    public static final String CONNECT_BUTTON_LABEL = "Connect";
     public final String UPDATE_LABEL = "Update";
     public static final String UPDATE_BUTTON_LABEL = "Update";
     private ProfileState state = new ProfileState();
