c6b1ce67b69618319413a4e2d965796c3250a566
Escalloniales <cyguchi@gmail.com>
2023-11-13 14:49:37 -0500

    revert input of createUser method

--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class UserFactory {
     public static User creatUser(String username,
-                          String password,
+                          String password, String id,
                           String email,
                           ArrayList<String> courses){
         return new User(username, password, null, email, courses);
