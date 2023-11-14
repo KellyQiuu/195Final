54be76824cccda434949a14ffde5afd510884a5a
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-14 15:24:00 -0500

    ViewModel.

--- src/interface_adapter/connect/ConnectViewModel.java
+++ src/interface_adapter/connect/ConnectViewModel.java
@@ -1,4 +1,14 @@
 package interface_adapter.connect;
 
 public class ConnectViewModel {
+    private String connectionStatus;
+
+    public String getConnectionStatus() {
+        return connectionStatus;
+    }
+
+    public void setConnectionStatus(String connectionStatus) {
+        this.connectionStatus = connectionStatus;
+        // Notify the view about the status change, for example through data binding or an observer
+    }
 }
