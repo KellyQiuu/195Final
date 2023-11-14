6b1856d010cbaab788ecc47ccf7646b58f98ed68
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-14 15:23:51 -0500

    Show success or not.

--- src/interface_adapter/connect/ConnectPresenter.java
+++ src/interface_adapter/connect/ConnectPresenter.java
@@ -1,4 +1,21 @@
 package interface_adapter.connect;
 
-public class ConnectPresenter {
+import use_case.connect.ConnectOutputBoundary;
+import use_case.connect.ConnectOutputData;
+
+public class ConnectPresenter implements ConnectOutputBoundary {
+    private final ConnectViewModel viewModel;
+
+    public ConnectPresenter(ConnectViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    @Override
+    public void onConnectionResult(ConnectOutputData outputData) {
+        if (outputData.isSuccess()) {
+            viewModel.setConnectionStatus("Email sent successfully.");
+        } else {
+            viewModel.setConnectionStatus("Failed to connect: " + outputData.getMessage());
+        }
+    }
 }
