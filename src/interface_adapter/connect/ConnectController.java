af7f8d6b0ea4644f44af6f94854481becee16ce0
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-14 15:20:39 -0500

    Already set the necessary functionalities.

--- src/interface_adapter/connect/ConnectController.java
+++ src/interface_adapter/connect/ConnectController.java
@@ -1,4 +1,24 @@
 package interface_adapter.connect;
 
+import use_case.connect.ConnectInputBoundary;
+import use_case.connect.ConnectInputData;
+
 public class ConnectController {
+    private final ConnectInputBoundary connectInputBoundary;
+
+    public ConnectController(ConnectInputBoundary connectInputBoundary) {
+        this.connectInputBoundary = connectInputBoundary;
+    }
+
+    public void onConnectButtonClicked(String senderUsername, String recipientEmail) {
+        // Trigger UI to show pop-up and get the free text message
+        String freeTextMessage = getFreeTextFromUser();
+        ConnectInputData inputData = new ConnectInputData(senderUsername, recipientEmail, freeTextMessage);
+        connectInputBoundary.handleConnect(inputData);
+    }
+
+    private String getFreeTextFromUser() {
+        // Implement the logic to show a pop-up and capture the free text from the user
+        return ""; // Return the message entered by the user
+    }
 }
