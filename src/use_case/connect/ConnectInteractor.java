ac6b8eb01276d067c4bff88f30f11b15c95c7162
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-14 15:23:03 -0500

    Handle the reaction while clicking "Connect" button, construct PersonalInfo.

---
+++ src/use_case/connect/ConnectInteractor.java
@@ -0,0 +1,43 @@
+package use_case.connect;
+
+import entity.User;
+import use_case.email_user.EmailService;
+
+public class ConnectInteractor implements ConnectInputBoundary {
+
+    private final ConnectOutputBoundary outputBoundary;
+    private final ConnectDataAccessInterface connectDataAccess;
+    private final EmailService emailService;
+
+    public ConnectInteractor(ConnectOutputBoundary outputBoundary,
+                             ConnectDataAccessInterface connectDataAccess,
+                             EmailService emailService) {
+        this.outputBoundary = outputBoundary;
+        this.connectDataAccess = connectDataAccess;
+        this.emailService = emailService;
+    }
+
+    @Override
+    public void handleConnect(ConnectInputData inputData) {
+        User sender = connectDataAccess.getUserByUsername(inputData.getSenderUsername());
+        if (sender == null) {
+            outputBoundary.onConnectionResult(new ConnectOutputData(false, "Sender user not found."));
+            return;
+        }
+
+        try {
+            String personalInfo = constructPersonalInfo(sender);
+            String emailContent = inputData.getMessage() + "\n\n" + personalInfo;
+            EmailService.sendEmail(inputData.getRecipientEmail(), "Connection Request", emailContent);
+            outputBoundary.onConnectionResult(new ConnectOutputData(true, "Email sent successfully."));
+        } catch (Exception e) {
+            outputBoundary.onConnectionResult(new ConnectOutputData(false, "Failed to send email: " + e.getMessage()));
+        }
+    }
+    private String constructPersonalInfo(User user) {
+        // Construct the personal information part of the email content
+        return "Name: " + user.getName() +
+                "\nEmail: " + user.getEmail() +
+                "\nCourses: " + String.join(", ", user.getCourses());
+    }
+}
