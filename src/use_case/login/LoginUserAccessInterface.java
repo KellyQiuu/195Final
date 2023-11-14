0a1aa36c81afa36fe39d9e48714c6cf25fab5e97
Escalloniales <cyguchi@gmail.com>
2023-11-13 19:04:10 -0500

    added login interactor

---
+++ src/use_case/login/LoginUserAccessInterface.java
@@ -0,0 +1,23 @@
+package use_case.login;
+
+import entity.User;
+
+/**
+ * Application Business Rules layer interface for accessing user data.
+ */
+
+public interface LoginUserAccessInterface {
+    /**
+     * @param username string username of the User to check if it has been taken.
+     * @return true if and only if the username has not been taken and the user can register with this name.
+     */
+    boolean existsByName(String username);
+
+    /**
+     * Saves a new user to the database.
+     * @param user newly created User object
+     */
+    void save(User user);
+
+    User get(String username);
+}
