b9a55658b61eacc1ba1268a3517414302c67a8d0
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-16 14:07:08 -0500

    main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,52 +1,32 @@
 package app;
 
-import use_case.SessionManagerInteractor;
-import entity.User;
-import entity.UserFactory;
+import data_access.UserDataAccessObject;
+import interface_adapter.connect.ConnectController;
+import view.ConnectView;
+import use_case.connect.ConnectInteractor;
+//import service.EmailService; // Replace with your actual email service implementation
+import use_case.connect.ConnectDataAccessInterface; // Replace with your actual data access implementation
 
-import java.util.ArrayList;
-import use_case.email_user.EmailService;
-
-
+import java.io.IOException;
 
 public class Main {
-    //Initialization
-    //// This setup code would typically go in your main method or application initialization logic
-    //
-    //// Create the dependencies of the ProfileController
-    //ProfileInputBoundary profileInteractor = new ProfileInteractor(/* dependencies */);
-    //EmailService emailService = new EmailService(/* your SendGrid API key */);
-    //ProfileView profileView = new ProfileView(/* ProfileViewModel */, /* ProfileController */);
-    //
-    //// Create the ProfileController
-    //ProfileController profileController = new ProfileController(profileInteractor, emailService, profileView);
-    //
-    //// Now that ProfileController is created, you must set it in the ProfileView
-    //profileView.setController(profileController);
-    public static void main(String[] args) throws Exception {
-        ArrayList<String> courses = new ArrayList<>();
 
-        User u=UserFactory.creatUser("User1","123","1","qiuwenyu2021@outlook.com",courses);
+    public static void main(String[] args) throws IOException {
+        // Create an instance of UserDataAccessObject
+        UserDataAccessObject dataAccess = new UserDataAccessObject(); // Replace with your constructor if different
 
+        // Create the ConnectInteractor instance
+        ConnectInteractor connectInteractor = new ConnectInteractor(dataAccess);
 
-        EmailService.sendEmail(u, freeTextMessage);
-        SessionManagerInteractor sessionManager = new SessionManagerInteractor();
-        User user = UserFactory.creatUser("Lynnesy", "wenyu.qiu@mail.utoronto.ca","0",
-                "wenyu.qiu@mail.utoronto.ca", new ArrayList<String>());
-        sessionManager.createUserSession(user);
+        // Assuming sender's username and recipient's email are known for demonstration
+        String senderUsername = "sender@example.com"; // Replace with actual username
+        String recipientEmail = "recipient@example.com"; // Replace with actual recipient email
 
-// When you need to retrieve the current user
-        User currentUser = sessionManager.getCurrentUser();
-        if (currentUser != null) {
-            // User is logged in
-            System.out.println("Current user ID: " + currentUser.getId());
-        } else {
-            // No user is currently logged in
-        }
+        // Create the ConnectController instance
+        ConnectController connectController = new ConnectController(connectInteractor, senderUsername, recipientEmail);
 
-// When the user logs out
-        sessionManager.endUserSession();
-
-
+        // Create and show the ConnectView
+        ConnectView connectView = new ConnectView(connectController);
+        connectView.setVisible(true); // Make sure to set the view visible
     }
 }
