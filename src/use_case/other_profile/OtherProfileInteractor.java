975718b75061f4ab0f731a0634abec18653e8830
Rogers <yanghaochong@gmail.com>
2023-11-15 20:21:17 -0500

    modified otherprofile to be extended by DAO

--- src/use_case/other_profile/OtherProfileInteractor.java
+++ src/use_case/other_profile/OtherProfileInteractor.java
@@ -1,6 +1,7 @@
 package use_case.other_profile;
 
 import entity.User;
+import entity.UserFactory;
 import use_case.self_profile.SelfProfileDataAccessInterface;
 import use_case.self_profile.SelfProfileOutputBoundary;
 import use_case.self_profile.SelfProfileOutputData;
@@ -21,5 +22,10 @@ public class OtherProfileInteractor implements OtherProfileInputBoundary{
 		User currentUser = profileDataAccessObject.getUser(otherProfileInputData.getUsername());
 		OtherProfileOutputData profileOutputData = new OtherProfileOutputData(currentUser);
 		profilePresenter.prepareSuccessView(profileOutputData);
+	}
+
+	private User censorUserData(User user) {
+		// Need to censor the user data : Password and ID.
+		return null;
 	}
 }
