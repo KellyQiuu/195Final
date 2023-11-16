417174dc378ffc2d008d7c07e4a6c2d804234610
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-16 13:29:34 -0500

    OutputData.

--- src/use_case/connect/ConnectOutputData.java
+++ src/use_case/connect/ConnectOutputData.java
@@ -7,15 +7,35 @@ public class ConnectOutputData {
     private final boolean success;
     private final String message;
 
+    /**
+     * Constructs a new ConnectOutputData instance.
+     *
+     * @param success Indicates whether the connection attempt was successful.
+     * @param message A message describing the result of the connection attempt.
+     * @throws IllegalArgumentException if message is null.
+     */
     public ConnectOutputData(boolean success, String message) {
+        if (message == null) {
+            throw new IllegalArgumentException("Message cannot be null.");
+        }
         this.success = success;
         this.message = message;
     }
 
+    /**
+     * Checks if the connection attempt was successful.
+     *
+     * @return true if successful, false otherwise.
+     */
     public boolean isSuccess() {
         return success;
     }
 
+    /**
+     * Gets the message describing the connection attempt result.
+     *
+     * @return The result message.
+     */
     public String getMessage() {
         return message;
     }
