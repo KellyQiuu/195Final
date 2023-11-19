56aee72f35e0b5c250d77dc56dff09e26cc3c84e
Escalloniales <cyguchi@gmail.com>
2023-11-19 01:45:24 -0500

    updated complete signup view

--- src/view/signup_login/SignupView.java
+++ src/view/signup_login/SignupView.java
@@ -29,26 +29,22 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
 
     private final JTextField emailInputField = new JTextField(15);
 
-//    private final JTextField course1InputField = new JTextField(5);
-//
-//    private final JTextField course2InputField = new JTextField(5);
-//
-//    private final JTextField course3InputField = new JTextField(5);
-//
-//    private final JTextField course4InputField = new JTextField(5);
-//
-//    private final JTextField course5InputField = new JTextField(5);
-
-
     private final JTextField[] courseInputFields = new JTextField[5];
+
+
+    private final JTextField course1InputField = new JTextField(8);
+
+    private final JTextField course2InputField = new JTextField(8);
+
+    private final JTextField course3InputField = new JTextField(8);
+
+    private final JTextField course4InputField = new JTextField(8);
+
+    private final JTextField course5InputField = new JTextField(8);
 
     private JButton signUp;
 
     private JButton cancel;
-
-//    private JFrame jFrame;
-
-
 
 
     public SignupView(SignupController controller,
@@ -58,198 +54,6 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
         signupViewModel.addPropertyChangeListener(this);
 
         initializeComponents();
-//        this.defaultValueInitialization();
-//        this.defaultComponentsInitialization();
-
-//        this.infFieldKey(); //for username, password, email
-//        this.courseFieldKey(); //for all five courses
-    }
-
-//    private void infFieldKey(){
-//        usernameInputField.addKeyListener(
-//                new KeyListener() {
-//                    @Override
-//                    public void keyTyped(KeyEvent e) {
-//                        SignupState currentState = signupViewModel.getState();
-//                        String text = usernameInputField.getText() + e.getKeyChar();
-//                        currentState.setUsername(text);
-//                        signupViewModel.setState(currentState);
-//                    }
-//
-//                    @Override
-//                    public void keyPressed(KeyEvent e) {}
-//
-//                    @Override
-//                    public void keyReleased(KeyEvent e) {}
-//                }
-//        );
-//
-//        passwordInputField.addKeyListener(
-//                new KeyListener() {
-//                    @Override
-//                    public void keyTyped(KeyEvent e) {
-//                        SignupState currentState = signupViewModel.getState();
-//                        String text = passwordInputField.getText() + e.getKeyChar();
-//                        currentState.setPassword(text);
-//                        signupViewModel.setState(currentState);
-//                    }
-//
-//                    @Override
-//                    public void keyPressed(KeyEvent e) {
-//                    }
-//
-//                    @Override
-//                    public void keyReleased(KeyEvent e) {
-//                    }
-//                }
-//        );
-//
-//        emailInputField.addKeyListener(
-//                new KeyListener() {
-//                    @Override
-//                    public void keyTyped(KeyEvent e) {
-//                        SignupState currentState = signupViewModel.getState();
-//                        String text = emailInputField.getText() + e.getKeyChar();
-//                        currentState.setEmail(text);
-//                        signupViewModel.setState(currentState);
-//                    }
-//
-//                    @Override
-//                    public void keyPressed(KeyEvent e) {
-//                    }
-//
-//                    @Override
-//                    public void keyReleased(KeyEvent e) {
-//                    }
-//                }
-//        );
-//    }
-//
-//    private void courseFieldKey(){
-//        course1InputField.addKeyListener(
-//                new KeyListener() {
-//                    @Override
-//                    public void keyTyped(KeyEvent e) {
-//                        SignupState currentState = signupViewModel.getState();
-//                        String text = course1InputField.getText() + e.getKeyChar();
-//                        currentState.setCourse1(text);
-//                        signupViewModel.setState(currentState);
-//                    }
-//
-//                    @Override
-//                    public void keyPressed(KeyEvent e) {}
-//
-//                    @Override
-//                    public void keyReleased(KeyEvent e) {}
-//                }
-//        );
-//
-//        course2InputField.addKeyListener(
-//                new KeyListener() {
-//                    @Override
-//                    public void keyTyped(KeyEvent e) {
-//                        SignupState currentState = signupViewModel.getState();
-//                        String text = course2InputField.getText() + e.getKeyChar();
-//                        currentState.setCourse2(text);
-//                        signupViewModel.setState(currentState);
-//                    }
-//
-//                    @Override
-//                    public void keyPressed(KeyEvent e) {}
-//
-//                    @Override
-//                    public void keyReleased(KeyEvent e) {}
-//                }
-//        );
-//
-//        course3InputField.addKeyListener(
-//                new KeyListener() {
-//                    @Override
-//                    public void keyTyped(KeyEvent e) {
-//                        SignupState currentState = signupViewModel.getState();
-//                        String text = course3InputField.getText() + e.getKeyChar();
-//                        currentState.setCourse3(text);
-//                        signupViewModel.setState(currentState);
-//                    }
-//
-//                    @Override
-//                    public void keyPressed(KeyEvent e) {}
-//
-//                    @Override
-//                    public void keyReleased(KeyEvent e) {}
-//                }
-//        );
-//
-//        course4InputField.addKeyListener(
-//                new KeyListener() {
-//                    @Override
-//                    public void keyTyped(KeyEvent e) {
-//                        SignupState currentState = signupViewModel.getState();
-//                        String text = course4InputField.getText() + e.getKeyChar();
-//                        currentState.setCourse4(text);
-//                        signupViewModel.setState(currentState);
-//                    }
-//
-//                    @Override
-//                    public void keyPressed(KeyEvent e) {}
-//
-//                    @Override
-//                    public void keyReleased(KeyEvent e) {}
-//                }
-//        );
-//
-//        course5InputField.addKeyListener(
-//                new KeyListener() {
-//                    @Override
-//                    public void keyTyped(KeyEvent e) {
-//                        SignupState currentState = signupViewModel.getState();
-//                        String text = course5InputField.getText() + e.getKeyChar();
-//                        currentState.setCourse5(text);
-//                        signupViewModel.setState(currentState);
-//                    }
-//
-//                    @Override
-//                    public void keyPressed(KeyEvent e) {}
-//
-//                    @Override
-//                    public void keyReleased(KeyEvent e) {}
-//                }
-//        );
-//    }
-
-//    private void defaultValueInitialization() {
-////        //set app name at the centre
-////        JLabel appName = new JLabel("195FinalProjectName");
-////        appName.setAlignmentX(Component.CENTER_ALIGNMENT);
-//
-//        int WIDTH = 800;
-//        int HEIGHT = 400;
-//
-//        // use frame?
-//        this.jFrame = new JFrame("195FinalProjectName");
-//        this.jFrame.setSize(WIDTH, HEIGHT);
-//        this.jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//    }
-
-    private void defaultComponentsInitialization(){
-        // Use GridBagLayout for arranging components
-        setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.insets = new Insets(10, 10, 10, 10); // Padding
-
-        //add signup and cancel button
-        JPanel buttons = new JPanel();
-        signUp = new JButton(SignupViewModel.SIGNUP_LABEL);
-        buttons.add(signUp, constraints);
-        constraints.gridx = 1;
-
-        cancel = new JButton(SignupViewModel.CANCEL_BUTTON_LABEL);
-        buttons.add(cancel, constraints);
-        constraints.gridx = 1;
-
     }
 
     private void initializeComponents() {
@@ -262,17 +66,32 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
         constraints.insets = new Insets(10, 10, 10, 10); // Padding
 
         //labels
-        addLabelAndField("Username:", new JTextField(15), constraints);
-        addLabelAndField("Password:", new JPasswordField(15), constraints);
-        addLabelAndField("Email:", new JTextField(15), constraints);
+        addLabelAndField("Username:", usernameInputField, constraints);
+        addLabelAndField("Password:", passwordInputField, constraints);
+        addLabelAndField("Email:", emailInputField, constraints);
+
+//        //course field in a single line
+//        JPanel coursePanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
+//        coursePanel.setBackground(new Color(30, 30, 30));
+//        for (int i = 1; i <= 5; i++) {
+//            addCourseField("Course " + i + ":", coursePanel);
+//        }
+//        add(coursePanel, constraints);
+//        constraints.gridy++;
+
 
         //course field in a single line
-        JPanel coursePanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
-        coursePanel.setBackground(new Color(30, 30, 30));
-        for (int i = 1; i <= 5; i++) {
-            addCourseField("Course " + i + ":", coursePanel);
-        }
-        add(coursePanel, constraints);
+        JPanel coursePanel2 = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
+        coursePanel2.setBackground(new Color(30, 30, 30));
+        addCourseField("Course 1:", course1InputField, coursePanel2);
+        addCourseField("Course 2:", course2InputField, coursePanel2);
+        addCourseField("Course 3:", course3InputField, coursePanel2);
+        addCourseField("Course 4:", course4InputField, coursePanel2);
+        addCourseField("Course 5:", course5InputField, coursePanel2);
+//        for (int i = 1; i <= 5; i++) {
+//            addCourseField("Course " + i + ":", coursePanel2);
+//        }
+        add(coursePanel2, constraints);
         constraints.gridy++;
 
 
@@ -280,28 +99,76 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
         JButton signUp = createButton("Sign Up", constraints);
         signUp.addActionListener(e -> handleSignUp());
         JButton cancel = createButton("Cancel", constraints);
+        // TODO: 11/19/2023
     }
 
     private void handleSignUp() {
         String username = usernameInputField.getText();
+
+        SignupState currentState = signupViewModel.getState();
+        currentState.setUsername(username);
+        signupViewModel.setState(currentState);
+
         String password = new String(passwordInputField.getPassword());
+        currentState.setPassword(password);
+        signupViewModel.setState(currentState);
+
+
         String email = emailInputField.getText();
+        currentState.setEmail(email);
+        signupViewModel.setState(currentState);
+
+
+        //=============== for 5 courses
         ArrayList<String> courses = new ArrayList<>();
-        for (JTextField courseField : courseInputFields) {
-            courses.add(courseField.getText());
-        }
+
+        String c1 = course1InputField.getText();
+        courses.add(c1);
+        currentState.setCourse1(c1);
+        signupViewModel.setState(currentState);
+
+        String c2 = course2InputField.getText();
+        courses.add(c2);
+        currentState.setCourse2(c2);
+        signupViewModel.setState(currentState);
+
+        String c3 = course3InputField.getText();
+        courses.add(c3);
+        currentState.setCourse3(c3);
+        signupViewModel.setState(currentState);
+
+        String c4 = course4InputField.getText();
+        courses.add(c4);
+        currentState.setCourse4(c4);
+        signupViewModel.setState(currentState);
+
+        String c5 = course5InputField.getText();
+        courses.add(c5);
+        currentState.setCourse5(c5);
+        signupViewModel.setState(currentState);
+
+        //================
+
+//        for (int i = 0; i < 5; i++) {
+//            courses.add(courseInputFields[i].getText());
+//        }
+
+//          ================================
+//        for (JTextField courseField : courseInputFields) {
+//            courses.add(courseField.getText());
+//        }
 
         // Pass data to the controller
         signupController.execute(username, password, "1", email, courses);
     }
 
-    private void addCourseField(String labelText, JPanel panel) {
+    private void addCourseField(String labelText, JTextField field, JPanel panel) {
         JLabel label = new JLabel(labelText);
         label.setForeground(Color.WHITE);
         label.setFont(new Font("Arial", Font.BOLD, 14));
-        JTextField field = new JTextField(8); // Half-width field
         field.setBackground(new Color(50, 50, 50));
         field.setForeground(Color.WHITE);
+        field.setColumns(8);
         panel.add(label);
         panel.add(field);
     }
