4b601fe224a8b193c6717ba42c4ea0ff89174268
Boddy161 <yige.yan@mail.utoronto.ca>
2023-11-20 16:11:29 -0500

    Data Transfer Object for carrying message data for the connection process.

--- src/use_case/connect/ConnectInputData.java
+++ src/use_case/connect/ConnectInputData.java
@@ -1,24 +1,25 @@
 package use_case.connect;
 
+/**
+ * Data Transfer Object for carrying message data for the connection process.
+ */
 public class ConnectInputData {
-    private final String senderUsername;
-    private final String recipientEmail;
-    private final String message;
+    private final String message; // Message content
 
-    public ConnectInputData(String senderUsername, String recipientEmail, String message) {
-        this.senderUsername = senderUsername;
-        this.recipientEmail = recipientEmail;
+    /**
+     * Constructs a new instance of ConnectInputData.
+     *
+     * @param message The message to be sent.
+     */
+    public ConnectInputData(String message) {
         this.message = message;
     }
 
-    public String getSenderUsername() {
-        return senderUsername;
-    }
-
-    public String getRecipientEmail() {
-        return recipientEmail;
-    }
-
+    /**
+     * Gets the message content.
+     *
+     * @return The message content.
+     */
     public String getMessage() {
         return message;
     }
